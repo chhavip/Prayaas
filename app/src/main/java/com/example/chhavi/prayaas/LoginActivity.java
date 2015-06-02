9853c6a0c476deebe7b3a553c72d351260d87a00
shikhark211 <shikhark211@gmail.com>
2015-06-03 01:31:34 +0530

    Adds the basic login page. Still lot of major work on login page left. Sample username:user password:password

---
+++ app/src/main/java/com/example/chhavi/prayaas/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.example.chhavi.prayaas;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends ActionBarActivity implements View.OnClickListener {
+
+    EditText username;
+    EditText password;
+    Button login;
+    Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = (Button) findViewById(R.id.login);
+        signUp = (Button) findViewById(R.id.signup);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login.setOnClickListener(this);
+        PrayaasSQLiteOpenHelper helper=new PrayaasSQLiteOpenHelper(this);
+        SQLiteDatabase db= helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(PrayaasContract.USER_TABLE_NAME_COL, "shikhar");
+        cv.put(PrayaasContract.USER_TABLE_USERNAME_COL, "user");
+        cv.put(PrayaasContract.USER_TABLE_PASSWORD_COL, "password");
+        cv.put(PrayaasContract.USER_TABLE_PHONE_COL, "9818115541");
+        cv.put(PrayaasContract.USER_TABLE_AGE_COL, 20);
+        cv.put(PrayaasContract.USER_TABLE_GENDER_COL, "Male");
+
+        db.insert(PrayaasContract.USER_TABLE, null, cv);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.login) {
+            if (username.getText().toString() == null || password.getText().toString() == null) {
+                Toast.makeText(this, "empty username/password", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String userName = username.getText().toString();
+            String passWord = password.getText().toString();
+
+            PrayaasSQLiteOpenHelper helper = new PrayaasSQLiteOpenHelper(this);
+            SQLiteDatabase db = helper.getReadableDatabase();
+
+            Cursor c = db.query(PrayaasContract.USER_TABLE, new String[]{PrayaasContract.USER_TABLE_USERNAME_COL, PrayaasContract.USER_TABLE_NAME_COL},
+
+                    PrayaasContract.USER_TABLE_USERNAME_COL + " = " + "'" + userName + "'"
+                            + " and " + PrayaasContract.USER_TABLE_PASSWORD_COL + " = "
+                            + "'" + passWord + "'", null, null, null, null, null);
+
+            Boolean right = c.moveToFirst();
+            if (right) {
+                Toast.makeText(this, "login successful", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent();
+                i.setClass(this, NavigationDrawer.class);
+                startActivity(i);
+            } else
+                Toast.makeText(this, "invalid username/password", Toast.LENGTH_SHORT).show();
+
+
+        }
+        else
+        {
+            Intent i = new Intent();
+            i.setClass(this, SignUpActivity.class);
+            startActivityForResult(i, 1);
+        }
+    }
+}
