4efc1fc82086573e302ef14c3e9cec4ff0778086
shikhark211 <shikhark211@gmail.com>
2015-06-03 19:30:52 +0530

    Adds the basic login page. Still lot of major work on login page left. Sample username:user password:password

---
+++ app/src/main/java/com/example/chhavi/prayaas/PrayaasContract.java
@@ -0,0 +1,24 @@
+package com.example.chhavi.prayaas;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by shikharkhetan on 6/2/15.
+ */
+public class PrayaasContract implements BaseColumns {
+
+    private PrayaasContract(){}
+
+    public static final String DB_NAME = "Prayaas";
+    public static final String USER_TABLE="User";
+    public static final String USER_TABLE_USERNAME_COL="User_Username";
+    public static final String USER_TABLE_PASSWORD_COL="Password";
+    public static final String USER_TABLE_NAME_COL="Name";
+    public static final String USER_TABLE_AGE_COL="Age";
+    public static final String USER_TABLE_PHONE_COL="Phone";
+    public static final String USER_TABLE_GENDER_COL="Gender";
+
+
+
+
+}
---
+++ app/src/main/java/com/example/chhavi/prayaas/PrayaasSQLiteOpenHelper.java
@@ -0,0 +1,32 @@
+package com.example.chhavi.prayaas;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by shikharkhetan on 6/3/15.
+ */
+public class PrayaasSQLiteOpenHelper extends SQLiteOpenHelper {
+    public PrayaasSQLiteOpenHelper(Context context) {
+        super(context, PrayaasContract.DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE User (USER_ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +PrayaasContract.USER_TABLE_NAME_COL+" TEXT,"
+                +PrayaasContract.USER_TABLE_AGE_COL+" INTEGER,"+
+                PrayaasContract.USER_TABLE_USERNAME_COL+" TEXT,"+
+                PrayaasContract.USER_TABLE_GENDER_COL+" TEXT,"+
+                PrayaasContract.USER_TABLE_PHONE_COL+" TEXT,"+
+                PrayaasContract.USER_TABLE_PASSWORD_COL+" TEXT"+
+
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
