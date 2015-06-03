a645da284bba2588015d5ed85af4b2b589242f3f
shikhark211 <shikhark211@gmail.com>
2015-06-03 15:45:29 +0530

    Adds the basic login page. Still lot of major work on login page left. Sample username:user password:password

--- app/src/main/java/com/example/chhavi/prayaas/LoginActivity.java
+++ app/src/main/java/com/example/chhavi/prayaas/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.chhavi.prayaas;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
@@ -20,16 +22,25 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
     EditText password;
     Button login;
     Button signUp;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sp = getSharedPreferences("Prayaas", Context.MODE_PRIVATE);
+//        if(sp != null)  {
+//            Intent i = new Intent();
+//            i.setClass(this, NavigationDrawer.class);
+//            startActivity(i);
+//        }
+
         login = (Button) findViewById(R.id.login);
         signUp = (Button) findViewById(R.id.signup);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         login.setOnClickListener(this);
+        signUp.setOnClickListener(this);
         PrayaasSQLiteOpenHelper helper=new PrayaasSQLiteOpenHelper(this);
         SQLiteDatabase db= helper.getWritableDatabase();
         ContentValues cv = new ContentValues();
@@ -82,7 +93,13 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
             PrayaasSQLiteOpenHelper helper = new PrayaasSQLiteOpenHelper(this);
             SQLiteDatabase db = helper.getReadableDatabase();
 
-            Cursor c = db.query(PrayaasContract.USER_TABLE, new String[]{PrayaasContract.USER_TABLE_USERNAME_COL, PrayaasContract.USER_TABLE_NAME_COL},
+            Cursor c = db.query(PrayaasContract.USER_TABLE, new String[]    {
+                            PrayaasContract.USER_TABLE_USERNAME_COL,
+                            PrayaasContract.USER_TABLE_NAME_COL,
+                            PrayaasContract.USER_TABLE_PASSWORD_COL,
+                            PrayaasContract.USER_TABLE_PHONE_COL,
+                            PrayaasContract.USER_TABLE_AGE_COL,
+                            PrayaasContract.USER_TABLE_GENDER_COL},
 
                     PrayaasContract.USER_TABLE_USERNAME_COL + " = " + "'" + userName + "'"
                             + " and " + PrayaasContract.USER_TABLE_PASSWORD_COL + " = "
@@ -91,6 +108,14 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
             Boolean right = c.moveToFirst();
             if (right) {
                 Toast.makeText(this, "login successful", Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("name", c.getString(c.getColumnIndex(PrayaasContract.USER_TABLE_NAME_COL)));
+                editor.putString("username", c.getString(c.getColumnIndex(PrayaasContract.USER_TABLE_USERNAME_COL)));
+                editor.putString("phone", c.getString(c.getColumnIndex(PrayaasContract.USER_TABLE_PHONE_COL)));
+                editor.putString("password", c.getString(c.getColumnIndex(PrayaasContract.USER_TABLE_PASSWORD_COL)));
+                editor.putString("gender", c.getString(c.getColumnIndex(PrayaasContract.USER_TABLE_GENDER_COL)));
+                editor.putInt("age", c.getInt(c.getColumnIndex(PrayaasContract.USER_TABLE_AGE_COL)));
+                editor.commit();
                 Intent i = new Intent();
                 i.setClass(this, NavigationDrawer.class);
                 startActivity(i);
@@ -99,7 +124,7 @@ public class LoginActivity extends ActionBarActivity implements View.OnClickList
 
 
         }
-        else
+        else if(v.getId() == R.id.signup)
         {
             Intent i = new Intent();
             i.setClass(this, SignUpActivity.class);
