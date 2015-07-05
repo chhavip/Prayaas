2c1d937a6d2465e1bc3c6a94731f3490889ab012
shikhark211 <shikhark211@gmail.com>
2015-07-05 23:33:04 +0530

    Latest code

--- app/src/main/java/com/example/chhavi/prayaas/AboutAppFragment.java
+++ app/src/main/java/com/example/chhavi/prayaas/AboutAppFragment.java
@@ -1,11 +1,15 @@
 package com.example.chhavi.prayaas;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.gc.materialdesign.views.ButtonRectangle;
 
 
 /**
@@ -24,6 +28,17 @@ public class AboutAppFragment extends android.support.v4.app.Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_about_app, container, false);
+        ButtonRectangle feedback = (ButtonRectangle) v.findViewById(R.id.feedBack);
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String recepientEmail = "prayaas.engio123@gmail.com"; // either set to destination email or leave empty
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                intent.setData(Uri.parse("mailto:" + recepientEmail));
+                startActivity(intent);
+            }
+        });
         return v;
     }
 
